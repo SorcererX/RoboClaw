9e8822ee0586c5a248a2d3a70d5ef17588a875e7
Kai Hugo Hustoft Endresen <kai.endresen@gmail.com>
2016-12-11 19:07:03 +0100

    Additional readouts in example
    - added some additional readouts

--- examples/info.cpp
+++ examples/info.cpp
@@ -6,13 +6,26 @@ int main()
     RoboClaw test( "/dev/ttyACM0", 5, 0x80 );
     char version[512];
     test.begin( B1152000 );
+
     test.ReadVersion( version );
     std::cout << "ReadVersion: " << version << std::flush;
+
     float Kp, Ki, Kd;
     uint32_t qpps;
+
     test.ReadM1VelocityPID( Kp, Ki, Kd, qpps );
-    std::cout << "M1 Velocity PID: (" << Kp << "," << Ki << "," << Kd << ") qpps:" << qpps << std::endl;
+    std::cout << "M1 Velocity PID: (" << Kp << "," << Ki << "," << Kd << ") QPPS:" << qpps << std::endl;
+
     test.ReadM2VelocityPID( Kp, Ki, Kd, qpps );
-    std::cout << "M2 Velocity PID: (" << Kp << "," << Ki << "," << Kd << ") qpps:" << qpps << std::endl;
+    std::cout << "M2 Velocity PID: (" << Kp << "," << Ki << "," << Kd << ") QPPS:" << qpps << std::endl;
+
+    bool valid;
+    uint16_t voltage = test.ReadMainBatteryVoltage( &valid );
+    std::cout << "Main Battery Voltage: " << voltage/10.0 << " V" << std::endl;
+
+    uint16_t temperature;
+    valid = test.ReadTemp( temperature );
+    std::cout << "Temperature: " << temperature/10.0 << " C" << std::endl;
+
     test.end();
 }
